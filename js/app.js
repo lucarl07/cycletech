8dda35d89d723cc7a6e9fb2a23b6c04babb1ad91
lucarl07 <junitinho2007@gmail.com>
2023-12-05 10:16:56 -0300

    Finalizar o funcionamento da barra lateral

--- js/app.js
+++ js/app.js
@@ -4,11 +4,20 @@ darkFilter = document.querySelector('#divDarkFilter'),
 mainContent = document.querySelector('.main');
 
 // Botões:
-const btnToggle = document.querySelector('#btnToggleSidebar');
+const btnOpenSidebar = document.querySelector('#btnOpenSidebar'),
+btnCloseSidebar = document.querySelector('#btnCloseSidebar'),
+toggleButtons = document.querySelectorAll('.toggle');
+
+// let isSidebarOpen = false;
 
 // Ao clicar #btnToggleSidebar.toggle:
-btnToggle.addEventListener('click', function() {
+btnOpenSidebar.addEventListener('click', function() {
     sidebar.style.display = 'initial'
     darkFilter.style.display = 'initial'
     mainContent.style.position = 'fixed';
+})
+btnCloseSidebar.addEventListener('click', function() {
+    sidebar.style.display = 'none'
+    darkFilter.style.display = 'none'
+    mainContent.style.position = 'initial';
 })
