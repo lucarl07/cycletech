fc2ab5a819cf7f65e14716829241c3218d06b40b
Pedro Lucas <133527072+PedroLucaV@users.noreply.github.com>
2023-12-07 08:53:22 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -69,38 +69,6 @@ subLogin.addEventListener('click', function(){
         }
 })
 
-subRegister.addEventListener('click', function(){
-        if(validaEmail(email, regexEmail) == false){
-            uncorrect.innerHTML = 'Invalido<br>Seu email deve ser neste modelo: meu@email.com'
-            validarEmail = false
-        }else{
-            uncorrect.innerText = ' '
-            console.log('passou do Email')
-            validarEmail = true
-        }
-
-        if(validaSenha(senha, regexSenha) == false){
-            uncorrectSenha.innerHTML = 'Invalido. Sua senha deve conter: <ul><li>Ao menos 8 digitos</li><li>Ao menos um caracter especial</li><li>Ao menos 1 número</li><li>Ao menos 1 letra maiuscula</li></ul>'
-            validarSenha = false
-        }else{
-            uncorrectSenha.innerText = ''
-            console.log('passou da Senha')
-            validarSenha = true
-        }
-
-        if(validaNome(nome) == false){
-            nameUncorrect.innerHTML = 'Invalido. Seu nome não pode estar vazio!'
-        }else{
-            nameUncorrect.innerText = ''
-            console.log('passou do nome')
-            validarNome = true
-        }
-
-        if(validarSenha == true && validarEmail == true && validarNome == true){
-            window.location.href = 'https://lucarl07.github.io/cycletech/pages/inicio.html'
-        }
-})
-
 function showPass() { 
     if(document.getElementById("senha").type == "password"){
         document.getElementById("senha").type = "text"
