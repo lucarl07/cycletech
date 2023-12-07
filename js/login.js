37bf55ec7db0976c738f746818b6b449a7beabeb
Pedro Lucas <133527072+PedroLucaV@users.noreply.github.com>
2023-12-07 08:45:26 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -4,6 +4,7 @@ const regexEmail = /^\S+@\S+\.\S+$/;
 const senha = document.getElementById('senha')
 const regexSenha = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#%¨&*_])[0-9a-zA-Z$*&@#%¨&*_]{8,}$/;
 const nome = document.getElementById('nome')
+const subRegister = document.getElementById('submitRegister')
 
 const nameUncorrect = document.getElementById('incorrect-name')
 const uncorrect = document.getElementById('incorrect')
@@ -37,6 +38,38 @@ function validaNome(nome){
 }
 
 subLogin.addEventListener('click', function(){
+        if(validaEmail(email, regexEmail) == false){
+            uncorrect.innerHTML = 'Invalido<br>Seu email deve ser neste modelo: meu@email.com'
+            validarEmail = false
+        }else{
+            uncorrect.innerText = ' '
+            console.log('passou do Email')
+            validarEmail = true
+        }
+
+        if(validaSenha(senha, regexSenha) == false){
+            uncorrectSenha.innerHTML = 'Invalido. Sua senha deve conter: <ul><li>Ao menos 8 digitos</li><li>Ao menos um caracter especial</li><li>Ao menos 1 número</li><li>Ao menos 1 letra maiuscula</li></ul>'
+            validarSenha = false
+        }else{
+            uncorrectSenha.innerText = ''
+            console.log('passou da Senha')
+            validarSenha = true
+        }
+
+        if(validaNome(nome) == false){
+            nameUncorrect.innerHTML = 'Invalido. Seu nome não pode estar vazio!'
+        }else{
+            nameUncorrect.innerText = ''
+            console.log('passou do nome')
+            validarNome = true
+        }
+
+        if(validarSenha == true && validarEmail == true){
+            window.location.href = 'https://lucarl07.github.io/cycletech/pages/inicio.html'
+        }
+})
+
+subRegister.addEventListener('click', function(){
         if(validaEmail(email, regexEmail) == false){
             uncorrect.innerHTML = 'Invalido<br>Seu email deve ser neste modelo: meu@email.com'
             validarEmail = false
